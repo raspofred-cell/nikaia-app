f7d67c0f07e1c4427381cd8c58a4864e2acaa97c
Fred <raspo.fred@gmail.com>
2026-02-19 10:43:48 +0100

    Déploiement du bandeau de consentement cookies

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import CookieConsent from "react-cookie-consent";
 import "@/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
@@ -5,6 +6,20 @@ import LandingPage from "./pages/LandingPage";
 function App() {
   return (
     <div className="dark">
+      <CookieConsent
+        debug={true}
+        location="bottom"
+        buttonText="Accepter"
+        declineButtonText="Refuser"
+        enableDeclineButton
+        cookieName="NikaIA-Consent-Final"
+        style={{ background: "#1a1a1a", color: "#fff", zIndex: "99999" }}
+        buttonStyle={{ background: "#ff0000", color: "#fff", borderRadius: "5px" }}
+        expires={150}
+      >
+        Ce site utilise des cookies pour analyser l'audience de NikaIA. 🍪
+      </CookieConsent>
+
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
